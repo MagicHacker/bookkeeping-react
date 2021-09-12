f7ae4f299d19ad7165cacba9ff0ae118fbf290ad
MagicHacker <zxmagichacker@gmail.com>
2021-09-12 21:58:31 +0800

    feat: 设置别名

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import styleImport from 'vite-plugin-style-import'
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), styleImport(
@@ -25,6 +26,12 @@ export default defineConfig({
         // 支持内联 JavaScript
         javascriptEnabled: true
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@utils': path.resolve(__dirname, 'src/utils')
     }
   }
 })
