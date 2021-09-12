9bd66377ec2050c03c64b52531dddf52362435ca
MagicHacker <zxmagichacker@gmail.com>
2021-09-12 23:54:59 +0800

    feat: 引入编写的tab-bar组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,15 @@
 import React, { useState } from 'react'
 import './App.css'
-
+import 'zarm/dist/zarm.css'
+import zhCN from 'zarm/lib/config-provider/locale/zh_CN';
+import { ConfigProvider} from 'zarm'
+import TabBar from './components/tab-bar'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <ConfigProvider locale={zhCN}>
+        <TabBar/>
+      </ConfigProvider>
     </div>
   )
 }
