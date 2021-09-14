52392190087a9840e82cda039b0596ce2b08a0ba
MagicHacker <zxmagichacker@gmail.com>
2021-09-14 22:10:06 +0800

    feat: 渲染路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 import './App.less'
 import 'zarm/dist/zarm.css'
 import zhCN from 'zarm/lib/config-provider/locale/zh_CN';
 import { ConfigProvider} from 'zarm'
 import TabBar from './components/tab-bar'
+import User from '@/pages/user'
+import Bill from '@/pages/bill'
+import Statistics from '@/pages/statistics'
 function App() {
   return (
     <div className="App">
-      <ConfigProvider locale={zhCN}>
-        <TabBar/>
-      </ConfigProvider>
+      <Router>
+        <ConfigProvider locale={zhCN}>
+          <Switch>
+            <Route path="/bill" component={Bill}></Route>
+            <Route path="/statistics" component={Statistics}></Route>
+            <Route path="/user" component={User}></Route>
+            <Redirect from="/" to="/bill"></Redirect>
+          </Switch>
+        </ConfigProvider>
+      </Router>
+      <TabBar/>
     </div>
   )
 }
