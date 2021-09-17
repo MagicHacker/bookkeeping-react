05997a99aff08d0675ee7fec5aac36a4f0585b5e
MagicHacker <zxmagichacker@gmail.com>
2021-09-17 23:15:30 +0800

    feat: 添加登录组件

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,18 @@ import TabBar from './components/tab-bar'
 import User from '@/pages/user'
 import Bill from '@/pages/bill'
 import Statistics from '@/pages/statistics'
+import Login from '@/pages/login'
 function App() {
   return (
     <div className="App">
       <Router>
         <ConfigProvider locale={zhCN}>
           <Switch>
+            <Route path="/login" component={Login}></Route>
             <Route path="/bill" component={Bill}></Route>
             <Route path="/statistics" component={Statistics}></Route>
             <Route path="/user" component={User}></Route>
-            <Redirect from="/" to="/bill"></Redirect>
+            <Redirect from="/" to="/login"></Redirect>
           </Switch>
         </ConfigProvider>
         <TabBar/>
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Tabs} from 'zarm'
+import './index.less'
 
 export default function Login() {
     return (
-        <div>
+        <div className="login-wrapper">
             登录页
         </div>
     )
