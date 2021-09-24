4df890f723d4c2752adcac23b30b37443deb6ca2
MagicHacker <zxmagichacker@gmail.com>
2021-09-24 23:32:29 +0800

    feat: 创建header组件

---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+/**
+ * 头部组件
+ */
+import React from 'react'
+import './index.less'
+export default function Header() {
+    return (
+        <div className="header-wrapper">
+            
+        </div>
+    )
+}
--- src/pages/bill/index.jsx
+++ src/pages/bill/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Header from '@/components/header'
+import './index.less'
 export default function Bill() {
     return (
         <div>
-            账单
+            <Header />
         </div>
     )
 }
