38036b5de0ae06e0919c07adb713f2ffc27ca59f
MagicHacker <zxmagichacker@gmail.com>
2021-09-25 17:52:52 +0800

    feat: 构建账单头部组件

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,36 @@
 /**
  * 头部组件
  */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Icon } from 'zarm'
 import './index.less'
 export default function Header() {
+    const [type, setType] = useState('全部类型')
+    const [time, setTime] = useState('')
+    useEffect(() => {
+        const date = new Date()
+        setTime(`${date.getFullYear()}-${date.getMonth() + 1}`)
+    }, [])
     return (
         <div className="header-wrapper">
-            
+            <div className="header-data-wrapper">
+                <span>总支出:
+                    <span className="header-data-number">￥0.00</span>
+                </span>&nbsp;&nbsp;
+                <span>总收入:
+                    <span className="header-data-number">￥0.00</span>
+                </span>
+            </div>
+            <div className="header-type-wrapper">
+                <div className="header-type-item">
+                    <span>{type}</span>
+                    <Icon className="header-type-icon" type="arrow-bottom" />
+                </div>
+                <div className="header-type-item">
+                    <span>{time}</span>
+                    <Icon className="header-type-icon" type="arrow-bottom" />
+                </div>
+            </div>
         </div>
     )
 }
