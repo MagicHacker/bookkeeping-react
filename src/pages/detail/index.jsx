0e6eee1d73ee6b6fb1769f263ff50506a5314f2a
MagicHacker <zxmagichacker@gmail.com>
2021-10-02 15:39:05 +0800

    feat: 添加删除模态框

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import BillHeader from '@/components/billHeader';
-import { Icon } from 'zarm';
+import { Icon, Modal } from 'zarm';
 import './index.less';
 export default function Detail() {
+  // 删除账单
+  const delBill = () => {
+    Modal.confirm({
+      title: '删除',
+      content: '确认删除？',
+      cancelText: '取消',
+      okText: '确认',
+      onOk: () => {
+        alert(1);
+      },
+    });
+  };
   return (
     <div className="bill-detail-wrapper">
       <BillHeader title="账单详情" />
@@ -25,7 +37,7 @@ export default function Detail() {
           </div>
         </div>
         <div className="bill-card-operation">
-          <span>
+          <span onClick={delBill}>
             <Icon type="shanchu" />
             删除
           </span>
