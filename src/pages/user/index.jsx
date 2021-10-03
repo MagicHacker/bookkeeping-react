8c9a80b3b4f194270f051f08b0690fcbaa7eff95
MagicHacker <zxmagichacker@gmail.com>
2021-10-03 23:17:32 +0800

    feat: 构建user模块

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Button, Cell } from 'zarm';
+import './index.less';
 
 export default function User() {
-  return <div>我的页</div>;
+  return (
+    <div className="user-wrapper">
+      <div className="user-header">
+        <div className="user-header-info">
+          <span>昵称：ss</span>
+          <span>
+            <img src="http://s.yezgea02.com/1615973630132/geqian.png" alt="" />
+            世界和平。
+          </span>
+        </div>
+        <img src="" alt="" />
+      </div>
+      <div className="user-content">
+        <Cell title="用户信息修改" hasArrow icon={<img src="http://s.yezgea02.com/1615974766264/gxqm.png" />} />
+        <Cell title="重置密码" hasArrow icon={<img src="http://s.yezgea02.com/1615974766264/zhaq.png" />} />
+      </div>
+      <Button theme="danger" className="user-logout">
+        退出登录
+      </Button>
+    </div>
+  );
 }
