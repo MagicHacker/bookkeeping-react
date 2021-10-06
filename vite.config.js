78777e34fad4f8e540083f668c0de35a93d15644
MagicHacker <zxmagichacker@gmail.com>
2021-10-06 22:41:21 +0800

    feat: 修改项目默认端口号

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import styleImport from 'vite-plugin-style-import';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8000,
+  },
   plugins: [
     reactRefresh(),
     styleImport({
